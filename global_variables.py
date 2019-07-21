a0197957566e16c5b50b7af28d55de5e37070528
Murphy Huang <huangyhq@gmail.com>
2019-07-21 19:38:57 -0400

    add fetch data function

---
+++ global_variables.py
@@ -0,0 +1,14 @@
+# encoding=utf-8
+
+import os
+
+
+# Selected pipeline
+pipeline = 'cpac'
+
+# Input data variables
+root_folder = '/tmp/abide/'
+data_folder = os.path.join(root_folder, 'ABIDE_pcp/cpac/filt_noglobal')
+phenotype = os.path.join(root_folder, 'ABIDE_pcp/Phenotypic_V1_0b_preprocessed1.csv')
+# num_subjects = 871  # Number of subjects
+num_subjects = 871
