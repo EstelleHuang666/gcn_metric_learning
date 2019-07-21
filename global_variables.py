a1d2a23f6757e5e9ff4ff3b28973398f2cf671cb
Murphy Huang <huangyhq@gmail.com>
2019-07-21 19:57:29 -0400

    add necessary files for fetch data

--- global_variables.py
+++ global_variables.py
@@ -7,7 +7,7 @@ import os
 pipeline = 'cpac'
 
 # Input data variables
-root_folder = '/tmp/abide/'
+root_folder = '/home/libi/HDD1/huan1282/tmp/abide/'
 data_folder = os.path.join(root_folder, 'ABIDE_pcp/cpac/filt_noglobal')
 phenotype = os.path.join(root_folder, 'ABIDE_pcp/Phenotypic_V1_0b_preprocessed1.csv')
 # num_subjects = 871  # Number of subjects
